3ee0e182bef8969cd9e9da88432d525df2aa7b80
Alexander Sierra <alexandercsierra@gmail.com>
2019-12-03 12:38:13 -0500

    got squares to move together

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -5,11 +5,35 @@ var red = document.querySelector('.block--red');
 var pink = document.querySelector('.block--pink');
 var gray = document.querySelector('.block--gray');
 var parent = document.querySelector('.blocks');
-var blocks = document.querySelectorAll(".block")
+var blocks = document.querySelectorAll(".block");
 
+var tween = gsap.to(".block", {
+    duration: 4, 
+    x: 750, 
+    rotation: 360, 
+    ease: "none", 
+    paused: true
+  });
+  
 
 blocks.forEach(block => {
     block.addEventListener("click", e => {
-    parent.insertBefore(block, parent.firstChild);
-    })
+        parent.insertBefore(block, parent.firstChild);     
+    });
+
+    block.addEventListener("mousedown", e => {
+        console.log("down");
+        tween.play();   
+    });
+
+    block.addEventListener("mouseleave", e => {
+        tween.pause();
+    });
+
+    block.addEventListener("mouseup", e => {
+        tween.pause();  
+    });
+
+
 });
+
